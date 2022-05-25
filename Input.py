2eaba0b002e28692334b82576a8d336fad38fa57
Jaap van der Stoel <j.p.v.d.stoel@student.tue.nl>
2022-05-25 15:03:05 +0200

    commit test

--- Input.py
+++ Input.py
@@ -1,2 +1,15 @@
-Class input:
-  pass
+from concurrent.futures.process import _ExecutorManagerThread
+
+
+class input:
+  
+  def __init__(self):
+    opp_loc =     []
+    opp_vel =     []
+    ball_loc =    []
+    peers_loc =   []
+    peers_vel =   []
+    
+
+
+
