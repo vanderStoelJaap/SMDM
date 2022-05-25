ec1e47a7234db9f6efec412b83a40035643884f5
Jaap van der Stoel <j.p.v.d.stoel@student.tue.nl>
2022-05-25 15:56:31 +0200

    change filename

---
+++ Representation.py
@@ -0,0 +1,24 @@
+import shapely
+import math
+
+class represitation(self): 
+
+    #ELEMENTARY BEHAVIORS
+    def skills(self):
+        def skills():
+        avoid = {"id" : "avoid"}
+        noEnter = {"id" : "noEnter"} # Necessary? 
+        avoidPass = {"id" : "avoidPass"}
+        move = {"id" : "Move"}
+        dribble = {"id" : "Dribble"}
+        humanDribble = {"id" : "HumanDribble"}
+        shoot = {"id" : "Shoot"}
+        pass = {"id" : "Pass"}
+        Target = {"id" : "Target"} # Necessary? 
+
+    #SHAPES 
+    def circle(self, pos, radius):
+        pass
+
+    def polygon(self):
+        pass
