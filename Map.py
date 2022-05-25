54e3c60154cf1512d27c6bccfad1331bd4785556
Jaap van der Stoel <j.p.v.d.stoel@student.tue.nl>
2022-05-25 18:01:25 +0200

    example map

--- Map.py
+++ Map.py
@@ -1,10 +1,14 @@
+from re import S
 import shapely.geometry as shape
 import matplotlib.pyplot as plt
 
 
 p = shape.Point(0,0)
 circ = p.buffer(2)
-
 x, y = circ.exterior.xy
-plt.plot(x,y) 
+square = shape.Polygon([[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]])
+x2, y2, = square.exterior.xy
+
+plt.plot(x,y)
+plt.plot(x2,y2) 
 plt.show()
