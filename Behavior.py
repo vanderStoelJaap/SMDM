970920fffc13ae2949e80e165df4720a9fb48b34
Jaap van der Stoel <j.p.v.d.stoel@student.tue.nl>
2022-05-25 15:02:50 +0200

    commit test

--- Behavior.py
+++ Behavior.py
@@ -1,2 +1,33 @@
+from tkinter import END
+from turtle import end_fill
+from Shapes import features
+
+
 class behavior:
-  pass
+  
+  def skills():
+    avoid = {"id" : "avoid"}
+    noEnter = {"id" : "noEnter"} # Necessary? 
+    avoidPass = {"id" : "avoidPass"}
+    Move = {"id" : "Move"}
+    Dribble = {"id" : "Dribble"}
+    HumanDribble = {"id" : "HumanDribble"}
+    Shoot = {"id" : "Shoot"}
+    Pass = {"id" : "Pass"}
+    Target = {"id" : "Target"} # Necessary? 
+
+  def semantics():
+     
+    avoidOpponentWide = []
+
+    avoidOpponentNarrow = []
+      for opponents in features.opponent:
+    
+    field = [] 
+
+    givepass = []
+
+    dribble = []
+
+    shoot = []
+
