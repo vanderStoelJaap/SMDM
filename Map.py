4c5e4bc82021df8d742e04b7565e1de9dc635a60
Jaap van der Stoel <j.p.v.d.stoel@student.tue.nl>
2022-05-25 15:03:11 +0200

    commit test

--- Map.py
+++ Map.py
@@ -1,2 +1,10 @@
-class Map:
-  pass
+import shapely.geometry as shape
+import matplotlib.pyplot as plt
+
+
+p = shape.Point(0,0)
+circ = p.buffer(2)
+
+x, y = circ.exterior.xy
+plt.plot(x,y) 
+plt.show()
