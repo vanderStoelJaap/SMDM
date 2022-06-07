d9115f66164f63b4d1e74c2c4dd64b2e9b991af6
Jaap van der Stoel <j.p.v.d.stoel@student.tue.nl>
2022-06-07 17:05:27 +0200

    update

--- Map.py
+++ Map.py
@@ -1,14 +1,38 @@
 from re import S
 import shapely.geometry as shape
 import matplotlib.pyplot as plt
+from Settings import Settings
+from Behavior import Behavior
+from Representation import Shape
+from Input import Feature, Opponent, Peer, Ball
 
 
-p = shape.Point(0,0)
-circ = p.buffer(2)
-x, y = circ.exterior.xy
-square = shape.Polygon([[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]])
-x2, y2, = square.exterior.xy
+field = Shape.rectangle([0,0], Settings.Field[1], Settings.Field[0], True)
+x, y = field.exterior.xy
+plt.plot(x,y, 'g')
 
-plt.plot(x,y)
-plt.plot(x2,y2) 
+for opponent in Opponent.all:
+    opp = Shape.circle(opponent.pos,Settings.TurtleDiameter)
+    x,y = opp.exterior.xy
+    plt.plot(x,y, 'm')
+
+for peer in Peer.all:
+    pr = Shape.circle(peer.pos,Settings.TurtleDiameter)
+    x,y = pr.exterior.xy
+    plt.plot(x,y, 'b')    
+    
+    
+for behavior in Behavior.all:
+    x,y = behavior.shape.exterior.xy
+    if behavior.label == "checkPassLine":
+        plt.plot(x,y, 'k')
+        print("label is checkPassLine")
+    if behavior.label == "avoidOpponent":
+        plt.plot(x,y, 'r')
+    else:
+        plt.plot(x,y, 'k')
+        print(behavior.label)
+
+print(Peer.all)
+print(Opponent.all)
 plt.show()
