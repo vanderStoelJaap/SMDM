062a991fe7f4121e98c4ba5940014f6a98a79b65
JaapvanderStoel <j.p.v.d.stoel@student.tue.nl>
2022-09-16 17:22:09 +0700

    update

--- Build.py
+++ Build.py
@@ -1,8 +1,9 @@
 from Features import Feature
 from Behavior import Behavior
+from potentialField import Potfield
 import time
 import Visualization
-
+from matplotlib import pyplot as plt
 start_time = time.time()
 
 role = 'main'
@@ -25,7 +26,28 @@ def run():
         Behavior.dribble()
     
     behaviors = Behavior.getAllBehavior()
+
+    k = 1
+
+    """
+    for behavior in behaviors: 
+        if behavior.shape: 
+            if k < 3: 
+                reg = behavior.shape
+                x , y = reg.exterior.xy
+                plt.plot(x,y) 
+                print(reg)
+                pmap, X, Y = Potfield.potentialfield(behavior.shape, "type")
+                print(pmap)
+                Visualization.vizPotField(pmap, X, Y)
+                k = k+1
+            else:  
+                print("Empty behavior")
+    """
+    
+    Visualization.nGridPoint(behaviors)
     Visualization.run(behaviors)
+    plt.show()
 
 Feature.instantiate_from_csv()
 run()
